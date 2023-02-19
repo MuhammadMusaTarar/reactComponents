855fcc9603a092a6048af878790b8e2c9a09ef8a
MuhammadMusaTarar <116386074+MuhammadMusaTarar@users.noreply.github.com>
2023-02-20 03:52:56 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Tweet from "./New_Comps/Tweet";
+
+function App() {
+  return Tweet();
+}
+
+export default App;
---
+++ src/New_Comps/Tweet.jsx
@@ -0,0 +1,12 @@
+export default Tweet;
+
+function Tweet() {
+
+  return (
+    <div>
+      <p>Tarar</p>
+      <p>I Will DO It INSHALLAH !!</p>
+    </div>
+  );
+  
+}
